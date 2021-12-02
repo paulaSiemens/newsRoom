100ad0667453b3e63447595892dc63ca051f91dd
Evaise20 <joh.englev@gmail.com>
2021-12-02 11:25:06 +0100

    moved routing back to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
-import MainContainer from "./components/MainContainer";
+import SideNav from "./components/SideNav";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
+import SubmitIdea from "./components/SubmitIdea";
+import Selection from "./components/Selection";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -27,9 +29,34 @@ function App() {
             path="/login"
             element={!Parse.User.current() ? <Login /> : <Navigate to="/" />}
           />
+          <Route
+            path="/submitIdea"
+            element={
+              Parse.User.current() ? (
+                <div className="mainContainer">
+                  <SideNav />
+                  <SubmitIdea />
+                </div>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
+          <Route
+            path="/selection"
+            element={
+              Parse.User.current() ? (
+                <div className="mainContainer">
+                  <SideNav />
+                  <Selection />
+                </div>
+              ) : (
+                <Navigate to="/login" />
+              )
+            }
+          />
         </Routes>
       </BrowserRouter>
-      <MainContainer />
       {/* <Footer /> */}
     </div>
   );
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -6,26 +6,7 @@ import SubmitIdea from "../components/SubmitIdea";
 import Selection from "../components/Selection";
 
 function MainContent() {
-  return (
-    <main className="mainContent">
-      <BrowserRouter>
-        <Routes>
-          <Route
-            path="/submitIdea"
-            element={
-              Parse.User.current() ? <SubmitIdea /> : <Navigate to="/login" />
-            }
-          />
-          <Route
-            path="/selection"
-            element={
-              Parse.User.current() ? <Selection /> : <Navigate to="/login" />
-            }
-          />
-        </Routes>
-      </BrowserRouter>
-    </main>
-  );
+  return <main className="mainContent"></main>;
 }
 
 export default MainContent
