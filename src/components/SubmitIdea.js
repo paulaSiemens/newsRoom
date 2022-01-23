ffc669a5e5eaa7a61b8dc305ed7cb5b01b588fef
seb4ever <sebastian@lajciak.com>
2022-01-23 16:39:34 +0100

    Add 3rd party API for generating random idea

--- src/components/SubmitIdea.js
+++ src/components/SubmitIdea.js
@@ -7,15 +7,44 @@ export default function SubmitIdea() {
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
 
+  function createRandomIdea(assignment) {
+    let theRandomNumber = Math.floor(Math.random() * 10) + 1;
+    let url =
+      "https://newsapi.org/v2/top-headlines?" +
+      "sources=bbc-news&" +
+      "apiKey=5d2b580655db4a1c8ad915b62f7908c3";
+
+    let req = new Request(url);
+    fetch(req).then((response) => {
+      response.json().then((result) => {
+        setTitle(result.articles[theRandomNumber].title);
+        setDescription(result.articles[theRandomNumber].description);
+      });
+    });
+  }
+
   return (
     <>
       <Form className="cont--submitIdea">
+        <Button
+          onClick={(e) => {
+            e.preventDefault();
+            createRandomIdea();
+          }}
+          variant="primary"
+          type="submit"
+        >
+          Generate random idea
+        </Button>
+        <br />
+        <br />
         {/* title and idea forms are mostly the same - should be refactored to avoid duplicating code  */}
         <Form.Group className="mb-3" controlId="formBasicUsername">
           <Form.Label>Title</Form.Label>
           <Form.Control
             type="text"
             onChange={(e) => setTitle(e.target.value)}
+            placeholder={title}
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -23,7 +52,7 @@ export default function SubmitIdea() {
           <Form.Control
             type="text"
             onChange={(e) => setDescription(e.target.value)}
-            placeholder="Start typing your idea here"
+            placeholder={description}
           />
         </Form.Group>
         <Button
